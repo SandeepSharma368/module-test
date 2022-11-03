9c4f74614548f04409e47fff676ce94cc9085c2c
gaurav260527 <114161968+gaurav260527@users.noreply.github.com>
2022-11-03 19:03:15 +0530

    Add files via upload

---
+++ versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  access_key = "AKIARWFXLG6LYVG7V5BA"
+  secret_key = "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"
+  region = "us-east-1"
+}
